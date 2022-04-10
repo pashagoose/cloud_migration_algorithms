13830b9c2c87a0d0644a2ee59df01396bd872ce6
pashagoose <pasha.guskov02@yandex.ru>
2022-04-10 18:04:55 +0300

    added more graphics

--- measurements/avg_hist.py
+++ measurements/avg_hist.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import common
+import sys
 
 def get_avg(ys):
 	return sum(ys) / len(ys)
 
-xs, ys_baseline = common.get_graphic_for_metrics('baseline_metrics.json')
-xs, ys_parallel_baseline = common.get_graphic_for_metrics('parallel_baseline_metrics.json')
-xs, ys_flow_grouping = common.get_graphic_for_metrics('parallel_flow_grouping.json')
-xs, ys_lowerbounds = common.get_graphic_for_lowerbounds('lowerbounds.txt')
+xs, ys_baseline = common.get_graphic_for_metrics(sys.argv[1])
+xs, ys_parallel_baseline = common.get_graphic_for_metrics(sys.argv[2])
+xs, ys_flow_grouping = common.get_graphic_for_metrics(sys.argv[3])
 
 avg_baseline = get_avg(ys_baseline)
 avg_parallel_baseline = get_avg(ys_parallel_baseline)
 avg_flow_grouping = get_avg(ys_flow_grouping)
 
 
-algo_names = ["baseline", "parallel_baseline", "flow_grouping"]
+algo_names = ["baseline", "parallel baseline", "flow grouping"]
 ys = [avg_baseline, avg_parallel_baseline, avg_flow_grouping]
 plt.bar(x=algo_names, height=ys, color=['r', 'g', 'b'])
 plt.show()
--- measurements/show_metrics.py
+++ measurements/show_metrics.py
@@ -7,7 +7,13 @@ xs, ys_parallel_baseline = common.get_graphic_for_metrics(sys.argv[2])
 xs, ys_flow_grouping = common.get_graphic_for_metrics(sys.argv[3])
 xs, ys_lowerbounds = common.get_graphic_for_lowerbounds(sys.argv[4])
 
-plt.plot(xs, ys_baseline, color='r', label='baseline')
+with_baseline = True
+if sys.argv[5] == 'without_baseline':
+	with_baseline = False
+
+if with_baseline:
+	plt.plot(xs, ys_baseline, color='r', label='baseline')
+
 plt.plot(xs, ys_parallel_baseline, color='g', label='parallel baseline')
 plt.plot(xs, ys_flow_grouping, color='b', label='flow grouping')
 plt.plot(xs, ys_lowerbounds, color='y', label='lowerbound')
